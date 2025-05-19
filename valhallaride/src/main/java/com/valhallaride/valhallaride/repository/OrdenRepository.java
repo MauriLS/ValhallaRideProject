43a2f00eeb83db3c6f2ef047e5e192a526597d26
JamesAVT <j.videlat@gmail.com>
2025-05-18 23:37:05 -0400

    Update OrdenRepository.java

--- valhallaride/src/main/java/com/valhallaride/valhallaride/repository/OrdenRepository.java
+++ valhallaride/src/main/java/com/valhallaride/valhallaride/repository/OrdenRepository.java
@@ -5,7 +5,7 @@ import com.valhallaride.valhallaride.model.Orden;
 import java.util.List;
 import java.sql.Date;
 
-public interface OrdenRepository extends JpaRepository<Orden, Long> {
+public interface OrdenRepository extends JpaRepository<Orden, Integer> {
 
  /*
  List <Orden> findByIdOrden(Integer idOrden); -> Esta no se usa ya que idOrden es clave primaria
