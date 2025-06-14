77ff3eebfa8b2e3b90572fac03acb4fa9f6315ab
JamesAVT <j.videlat@gmail.com>
2025-06-13 23:20:14 -0400

    Update Tienda.java

--- valhallaride/src/main/java/com/valhallaride/valhallaride/model/Tienda.java
+++ valhallaride/src/main/java/com/valhallaride/valhallaride/model/Tienda.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
